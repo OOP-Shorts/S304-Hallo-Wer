ddcf23ba21a9386468c1ef842842591de0cf8734
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-22 17:17:35 +0100

    Add solution

--- src/HalloWer.java
+++ src/HalloWer.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class HalloWer {
 
-    public static void main(String[] args) {
+    public static String readName() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Bitte gib deinen Namen ein: ");
+        return console.nextLine();
+    }
 
+    public static void main(String[] args) {
+        String name = readName();
+        System.out.println("Hello " + name);
     }
 }
